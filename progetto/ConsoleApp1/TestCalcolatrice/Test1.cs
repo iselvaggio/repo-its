bd82be4d051ee8c14d6644a1e029f5f8c48646b7
IgnazioSelvaggio <49954442+IgnazioSelvaggio@users.noreply.github.com>
2025-06-25 15:59:19 +0200

    #1 nuovo commit con test

--- progetto/ConsoleApp1/TestCalcolatrice/Test1.cs
+++ progetto/ConsoleApp1/TestCalcolatrice/Test1.cs
@@ -12,7 +12,7 @@ namespace TestCalcolatrice
 
             Calcolatrice calcolatrice = new Calcolatrice();
             double risultato = calcolatrice.Somma(2, 3);
-            Assert.AreEqual(5, risultato, "La somma di 2 e 3 dovrebbe essere 5.");
+            Assert.AreEqual(3, risultato, "La somma di 2 e 3 dovrebbe essere 5.");
 
         }
         [TestMethod]
