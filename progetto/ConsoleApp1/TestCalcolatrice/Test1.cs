4b0ac3b116fd3a965494b422477a387a5a813a08
IgnazioSelvaggio <49954442+IgnazioSelvaggio@users.noreply.github.com>
2025-06-25 15:31:52 +0200

    #1 modifica progetto calcolatrice

---
+++ progetto/ConsoleApp1/TestCalcolatrice/Test1.cs
@@ -0,0 +1,27 @@
+﻿using CalcolatriceLib;
+
+namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            //crea test per metodo Somma della classe Calcolatrice
+
+            Calcolatrice calcolatrice = new Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato, "La somma di 2 e 3 dovrebbe essere 5.");
+
+        }
+        [TestMethod]
+        public void TestSommaConZero()
+        {
+            //crea test per metodo Somma della classe Calcolatrice con uno dei due parametri uguale a zero
+            Calcolatrice calcolatrice = new Calcolatrice();
+            double risultato = calcolatrice.Somma(0, 3);
+            Assert.AreEqual(3, risultato, "La somma di 0 e 3 dovrebbe essere 3.");
+        }
+    }
+}
